ee876f16219851e61a1f967940c500c5d70aa142
EC2 Default User <ec2-user@ip-172-31-26-146.eu-west-1.compute.internal>
2023-11-19 19:53:27 +0000

    changed to use FunnyUniqueAppRunnerServiceRole and FunnyUniqueAppRunnerPolicy

--- infra/main.tf
+++ infra/main.tf
@@ -2,14 +2,14 @@ resource "aws_apprunner_service" "apprunner_service" {
   service_name = var.service_name
 
   instance_configuration {
-    instance_role_arn = aws_iam_role.apprunner_service_role.arn
+    instance_role_arn = data.aws_iam_role.apprunner_service_role.arn
     cpu               = 256
     memory            = 1024
   }
 
   source_configuration {
     authentication_configuration {
-      access_role_arn = aws_iam_role.apprunner_service_role.arn
+      access_role_arn = data.aws_iam_role.apprunner_service_role.arn
     }
     image_repository {
       image_configuration {
@@ -22,51 +22,15 @@ resource "aws_apprunner_service" "apprunner_service" {
   }
 }
 
-resource "aws_iam_role" "apprunner_service_role" {
-  name               = "FunnyUniqueAppRunnerServiceRole"
-  assume_role_policy = data.aws_iam_policy_document.apprunner_assume_role.json
+data "aws_iam_role" "apprunner_service_role" {
+  name = "FunnyUniqueAppRunnerServiceRole"
 }
 
-data "aws_iam_policy_document" "apprunner_assume_role" {
-  statement {
-    effect = "Allow"
-
-    principals {
-      type        = "Service"
-      identifiers = ["tasks.apprunner.amazonaws.com"]
-    }
-
-    actions = ["sts:AssumeRole"]
-  }
-}
-
-data "aws_iam_policy_document" "apprunner_policy" {
-  statement {
-    effect    = "Allow"
-    actions   = ["rekognition:*"]
-    resources = ["*"]
-  }
-  
-  statement {
-    effect    = "Allow"
-    actions   = ["s3:*"]
-    resources = ["*"]
-  }
-
-  statement {
-    effect    = "Allow"
-    actions   = ["cloudwatch:*"]
-    resources = ["*"]
-  }
-}
-
-resource "aws_iam_policy" "apprunner_policy" {
-  name        = "FunnyUniqueAppRunnerPolicy"
-  description = "Policy for AWS App Runner service instance"
-  policy      = data.aws_iam_policy_document.apprunner_policy.json
+data "aws_iam_policy" "apprunner_policy" {
+  name = "FunnyUniqueAppRunnerPolicy"
 }
 
 resource "aws_iam_role_policy_attachment" "policy_attachment" {
-  role       = aws_iam_role.apprunner_service_role.name
-  policy_arn = aws_iam_policy.apprunner_policy.arn
+  role       = data.aws_iam_role.apprunner_service_role.name
+  policy_arn = data.aws_iam_policy.apprunner_policy.arn
 }
