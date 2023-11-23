758de2409361b5483e043cd93db50d173590d577
EC2 Default User <ec2-user@ip-172-31-26-146.eu-west-1.compute.internal>
2023-11-23 22:58:05 +0000

    cooking even more crazyu

--- infra/main.tf
+++ infra/main.tf
@@ -11,18 +11,55 @@ provider "aws" {
   region = "eu-west-1"
 }
 
+data "aws_iam_policy_document" "assume_role" {
+  statement {
+    effect = "Allow"
+
+    principals {
+      type        = "Service"
+      identifiers = ["apprunner.amazonaws.com"]
+    }
+
+    actions = ["sts:AssumeRole"]
+  }
+}
+
+data "aws_iam_policy_document" "policy" {
+  statement {
+    effect    = "Allow"
+    actions   = ["rekognition:*", "s3:*", "cloudwatch:*"]
+    resources = ["*"]
+  }
+}
+
+resource "aws_iam_role" "role_for_apprunner_service" {
+  name               = "2029-crazy-cool-role"
+  assume_role_policy = data.aws_iam_policy_document.assume_role.json
+}
+
+resource "aws_iam_policy" "policy" {
+  name        = "2029-crazy-cool-policy"
+  description = "Policy for AWS App Runner Service"
+  policy      = data.aws_iam_policy_document.policy.json
+}
+
+resource "aws_iam_role_policy_attachment" "attachment" {
+  role       = aws_iam_role.role_for_apprunner_service.name
+  policy_arn = aws_iam_policy.policy.arn
+}
+
 resource "aws_apprunner_service" "service" {
   service_name = var.service_name
 
   instance_configuration {
-    instance_role_arn = "arn:aws:iam::244530008913:role/service-role/AppRunnerECRAccessRole"
+    instance_role_arn = aws_iam_role.role_for_apprunner_service.arn
     cpu               = 256
     memory            = 1024
   }
 
   source_configuration {
     authentication_configuration {
-      access_role_arn = "arn:aws:iam::244530008913:role/service-role/AppRunnerECRAccessRole"
+      access_role_arn = aws_iam_role.role_for_apprunner_service.arn
     }
     image_repository {
       image_configuration {
