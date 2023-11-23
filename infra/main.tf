18938b4e5ae807e8392bdb14cd816640c21c632c
EC2 Default User <ec2-user@ip-172-31-26-146.eu-west-1.compute.internal>
2023-11-23 23:11:14 +0000

    cookington city

--- infra/main.tf
+++ infra/main.tf
@@ -11,13 +11,44 @@ provider "aws" {
   region = "eu-west-1"
 }
 
+resource "aws_apprunner_service" "service" {
+  service_name = var.service_name
+
+  instance_configuration {
+    instance_role_arn = aws_iam_role.role_for_apprunner_service.arn
+    
+    cpu = 256
+    memory = 1024
+  }
+
+  source_configuration {
+    authentication_configuration {
+      access_role_arn = "arn:aws:iam::244530008913:role/service-role/AppRunnerECRAccessRole"
+    }
+    image_repository {
+      image_configuration {
+        port = "8080"
+      }
+      image_identifier      = var.image_identifier
+      image_repository_type = "ECR"
+    }
+    auto_deployments_enabled = true
+  }
+}
+
+resource "aws_iam_role" "role_for_apprunner_service" {
+  name               = "2029-apprunner-role-unique"
+  assume_role_policy = data.aws_iam_policy_document.assume_role.json
+}
+
+
 data "aws_iam_policy_document" "assume_role" {
   statement {
     effect = "Allow"
 
     principals {
       type        = "Service"
-      identifiers = ["apprunner.amazonaws.com"]
+      identifiers = ["tasks.apprunner.amazonaws.com"]
     }
 
     actions = ["sts:AssumeRole"]
@@ -27,47 +58,32 @@ data "aws_iam_policy_document" "assume_role" {
 data "aws_iam_policy_document" "policy" {
   statement {
     effect    = "Allow"
-    actions   = ["rekognition:*", "s3:*", "cloudwatch:*"]
+    actions   = ["rekognition:*"]
+    resources = ["*"]
+  }
+  
+  statement  {
+    effect    = "Allow"
+    actions   = ["s3:*"]
+    resources = ["*"]
+  }
+
+  statement  {
+    effect    = "Allow"
+    actions   = ["cloudwatch:*"]
     resources = ["*"]
   }
 }
 
-resource "aws_iam_role" "role_for_apprunner_service" {
-  name               = "2029-crazy-cool-role"
-  assume_role_policy = data.aws_iam_policy_document.assume_role.json
-}
-
 resource "aws_iam_policy" "policy" {
-  name        = "2029-crazy-cool-policy"
-  description = "Policy for AWS App Runner Service"
+  name        = "2029-apprunner-policy-unique"
+  description = "test"
   policy      = data.aws_iam_policy_document.policy.json
 }
 
+
 resource "aws_iam_role_policy_attachment" "attachment" {
-  role       = aws_iam_role.role_for_apprunner_service.name
-  policy_arn = aws_iam_policy.policy.arn
-}
-
-resource "aws_apprunner_service" "service" {
-  service_name = var.service_name
-
-  instance_configuration {
-    instance_role_arn = aws_iam_role.role_for_apprunner_service.arn
-    cpu               = 256
-    memory            = 1024
-  }
-
-  source_configuration {
-    authentication_configuration {
-      access_role_arn = aws_iam_role.role_for_apprunner_service.arn
-    }
-    image_repository {
-      image_configuration {
-        port = "8080"
-      }
-      image_identifier      = var.img
-      image_repository_type = "ECR"
-    }
-    auto_deployments_enabled = true
-  }
+  role        = aws_iam_role.role_for_apprunner_service.name
+  description = "test"
+  policy_arn  = aws_iam_policy.policy.arn
 }
