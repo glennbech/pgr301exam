cd7e6c1e51ce257006ab2595ab71e693bdb12d27
EC2 Default User <ec2-user@ip-172-31-26-146.eu-west-1.compute.internal>
2023-11-23 18:08:35 +0000

    made terraform make a new role if it doesnt exist or use existing one, change data to resource

--- infra/main.tf
+++ infra/main.tf
@@ -1,15 +1,61 @@
+resource "aws_iam_role" "apprunner_service_role" {
+  name = "apprunner-2029-role"
+
+  assume_role_policy = jsonencode({
+    Version = "2012-10-17",
+    Statement = [
+      {
+        Action = "sts:AssumeRole",
+        Effect = "Allow",
+        Principal = {
+          Service = "build.apprunner.amazonaws.com"
+        },
+      },
+    ],
+  })
+}
+
+resource "aws_iam_policy" "apprunner_policy" {
+  name        = "apprunner-2029-policy"
+  description = "Policy for AWS App Runner Service"
+
+  policy = jsonencode({
+    Version = "2012-10-17",
+    Statement = [
+      {
+        Action = [
+          "ecr:BatchCheckLayerAvailability",
+          "ecr:GetDownloadUrlForLayer",
+          "ecr:BatchGetImage",
+          "logs:CreateLogStream",
+          "logs:PutLogEvents",
+          "logs:CreateLogGroup",
+          "ecr:GetAuthorizationToken",
+        ],
+        Effect   = "Allow",
+        Resource = "*",
+      },
+    ],
+  })
+}
+
+resource "aws_iam_role_policy_attachment" "policy_attachment" {
+  role       = aws_iam_role.apprunner_service_role.name
+  policy_arn = aws_iam_policy.apprunner_policy.arn
+}
+
 resource "aws_apprunner_service" "apprunner_service" {
   service_name = var.service_name
 
   instance_configuration {
-    instance_role_arn = data.aws_iam_role.apprunner_service_role.arn
+    instance_role_arn = aws_iam_role.apprunner_service_role.arn
     cpu               = 256
     memory            = 1024
   }
 
   source_configuration {
     authentication_configuration {
-      access_role_arn = data.aws_iam_role.apprunner_service_role.arn
+      access_role_arn = aws_iam_role.apprunner_service_role.arn
     }
     image_repository {
       image_configuration {
@@ -20,17 +66,4 @@ resource "aws_apprunner_service" "apprunner_service" {
     }
     auto_deployments_enabled = true
   }
-}
-
-data "aws_iam_role" "apprunner_service_role" {
-  name = "FunnyUniqueAppRunnerServiceRole"
-}
-
-data "aws_iam_policy" "apprunner_policy" {
-  name = "FunnyUniqueAppRunnerPolicy"
-}
-
-resource "aws_iam_role_policy_attachment" "policy_attachment" {
-  role       = data.aws_iam_role.apprunner_service_role.name
-  policy_arn = data.aws_iam_policy.apprunner_policy.arn
 }
