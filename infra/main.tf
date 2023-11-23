b409cbaf2ccfe1cb7558067cf3140c7654c6e9a7
EC2 Default User <ec2-user@ip-172-31-26-146.eu-west-1.compute.internal>
2023-11-23 22:16:04 +0000

    changed to use apprunnerecraccessrole

--- infra/main.tf
+++ infra/main.tf
@@ -1,28 +1,15 @@
-data "aws_iam_role" "apprunner_service_role" {
-  name = "apprunner-2029-role"
-}
-
-data "aws_iam_policy" "apprunner_policy" {
-  name = "apprunner-2029-policy"
-}
-
-resource "aws_iam_role_policy_attachment" "policy_attachment" {
-  role       = data.aws_iam_role.apprunner_service_role.name
-  policy_arn = data.aws_iam_policy.apprunner_policy.arn
-}
-
 resource "aws_apprunner_service" "apprunner_service" {
   service_name = var.service_name
 
   instance_configuration {
-    instance_role_arn = data.aws_iam_role.apprunner_service_role.arn
+    instance_role_arn = "arn:aws:iam::244530008913:role/service-role/AppRunnerECRAccessRole"
     cpu               = 256
     memory            = 1024
   }
 
   source_configuration {
     authentication_configuration {
-      access_role_arn = data.aws_iam_role.apprunner_service_role.arn
+      access_role_arn = "arn:aws:iam::244530008913:role/service-role/AppRunnerECRAccessRole"
     }
     image_repository {
       image_configuration {
