0fa7e9dd939fd30834dd3e24fbded111c241d612
EC2 Default User <ec2-user@ip-172-31-26-146.eu-west-1.compute.internal>
2023-11-28 04:19:22 +0000

    minor change

--- infra/alarm.tf
+++ infra/alarm.tf
@@ -1,5 +1,11 @@
-variable "namespace" {
-  type = string
+resource "aws_sns_topic" "alarm_topic" {
+  name = "alarm-topic"
+}
+
+resource "aws_sns_topic_subscription" "alarm_subscription" {
+  topic_arn = aws_sns_topic.alarm_topic.arn
+  protocol  = "email"
+  endpoint  = var.notification_email
 }
 
 resource "aws_cloudwatch_metric_alarm" "violation_alarm" {
@@ -7,11 +13,11 @@ resource "aws_cloudwatch_metric_alarm" "violation_alarm" {
   comparison_operator = "GreaterThanOrEqualToThreshold"
   evaluation_periods  = 1
   metric_name         = "violations.true.count"
-  namespace           = var.namespace
+  namespace           = "testnamespace2029"
   period              = 60
   statistic           = "Sum"
   threshold           = 1
   alarm_description   = "Alarm when the number of true violations is 1 or more"
-  actions_enabled     = false
-
+  actions_enabled     = true
+  alarm_actions       = [aws_sns_topic.alarm_topic.arn]
 }
